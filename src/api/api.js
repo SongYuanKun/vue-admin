67ffd9b680563f4ed54dbc58492934d2a49fd5f2
songyuankun <songyuankun@huohua.cn>
2019-07-23 22:32:19 +0800

    完成登录功能

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-let base = '';
+let base = 'http://localhost:8082';
 
-export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+export const requestLogin = params => { return axios.post(`${base}/loginByPassword`, params).then(res => res.data); };
 
 export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
 
