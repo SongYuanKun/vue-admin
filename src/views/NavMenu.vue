869f04d29d63113d4c3992131f6043ae37520588
songyuankun <songyuankun@huohua.cn>
2019-07-27 11:18:43 +0800

    跳整菜单

--- src/views/NavMenu.vue
+++ src/views/NavMenu.vue
@@ -1,18 +1,21 @@
-<template v-for="(item,index) in navMenus" v-if="!item.hidden">
-    <el-submenu :index="index+''" v-if="!item.leaf">
-        <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-        <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path"
-                      v-if="!child.hidden">{{child.name}}
-        </el-menu-item>
-    </el-submenu>
-    <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i
-            :class="item.iconCls"></i>{{item.children[0].name}}
-    </el-menu-item>
+<template>
+    <div>
+        <template v-for="(navMenus,index) in navMenus" v-if="!navMenus.hidden">
+            <el-submenu :index="index+''" v-if="!navMenus.leaf">
+                <template slot="title"><i :class="navMenus.iconCls"></i>{{navMenus.name}}</template>
+                <el-menu-item v-for="child in navMenus.children" :index="child.path" :key="child.path"
+                              v-if="!child.hidden">{{child.name}}
+                </el-menu-item>
+            </el-submenu>
+            <el-menu-item v-if="navMenus.leaf&&navMenus.children.length>0" :index="navMenus.children[0].path"><i
+                    :class="navMenus.iconCls"></i>{{navMenus.children[0].name}}
+            </el-menu-item>
+        </template>
+    </div>
 </template>
-
 <script>
     export default {
-        name: 'NavMenu',
+        name: 'navMenus',
         props: ['navMenus'],
         data() {
             return {}
